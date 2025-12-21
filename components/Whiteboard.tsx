de68ddd8daca1055b7aaa66b9bee77821d7d7a15
MohammedAmr04 <mohammedamr200497@gmail.com>
2025-12-21 15:19:06 +0200

    feat: Remove Selected Id to can move layers well

--- components/Whiteboard.tsx
+++ components/Whiteboard.tsx
@@ -1,13 +1,26 @@
 "use client";
 import { useWhiteboard } from "@/store";
 import { SHAPE_COMPONENTS } from "@/types/types";
+import Konva from "konva";
 import { Layer, Stage } from "react-konva";
 
 export default function Whiteboard() {
   const { layers, setSelectedLayerId } = useWhiteboard();
 
+  const handleCheckDeselect = (e: Konva.KonvaEventObject<Event>) => {
+    const { target } = e;
+    const clickedOnEmpty = target === target.getStage();
+    if (clickedOnEmpty) {
+      setSelectedLayerId(null);
+    }
+  };
   return (
-    <Stage width={window.innerWidth} height={window.innerHeight}>
+    <Stage
+      width={window.innerWidth}
+      height={window.innerHeight}
+      onMouseDown={handleCheckDeselect}
+      onTouchStart={handleCheckDeselect}
+    >
       <Layer>
         {layers.map((layer) => {
           const Component = SHAPE_COMPONENTS[layer.type];
@@ -17,6 +30,7 @@ export default function Whiteboard() {
               key={layer.id}
               {...layer}
               onClick={() => setSelectedLayerId(layer.id)}
+              onTap={() => setSelectedLayerId(layer.id)}
             />
           );
         })}
